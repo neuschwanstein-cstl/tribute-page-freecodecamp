f72ce96af529facb4b9be794054a62372ae4b962
neus <akishinonomiyaxhic@gmail.com>
2023-05-06 18:51:35 +0800

    Change Indent to 4 spaces!!!

--- app.js
+++ app.js
@@ -7,25 +7,25 @@ const hamIcon = document.querySelector(".fa-bars");
 console.log(menuItem);
 //Icon style toggle
 const toggleIcon = function () {
-  if (myNav.classList.contains("responsive")) {
-    hamIcon.classList.remove("fa-bars");
-    hamIcon.classList.add("fa-times");
-  } else {
-    hamIcon.classList.add("fa-bars");
-    hamIcon.classList.remove("fa-times");
-  }
+    if (myNav.classList.contains("responsive")) {
+        hamIcon.classList.remove("fa-bars");
+        hamIcon.classList.add("fa-times");
+    } else {
+        hamIcon.classList.add("fa-bars");
+        hamIcon.classList.remove("fa-times");
+    }
 };
 
 //Menu toggle by clicking on icon
 myHamburger.addEventListener("click", function () {
-  myNav.classList.toggle("responsive");
-  toggleIcon();
+    myNav.classList.toggle("responsive");
+    toggleIcon();
 });
 
 //Menu toggle by clicking on menu item
 menuItem.forEach(function (e) {
-  e.addEventListener("click", function () {
-    myNav.classList.remove("responsive");
-    toggleIcon();
-  });
+    e.addEventListener("click", function () {
+        myNav.classList.remove("responsive");
+        toggleIcon();
+    });
 });
